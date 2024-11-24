cd5be15ffac563a650d8ec664a536e7e51b99dce
alkrona <alkrona117@gmail.com>
2024-11-24 11:15:33 +1100

    added correct path to preview file

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import pandas as pd
 import requests
 from call_api import send_api_request
 st.write("# Chat with Property_data Data ")
-df = pd.read_csv("data/suppliers.csv")
+df = pd.read_csv("data/property_data.csv")
 with st.expander(" Dataframe Preview"):
-    st.write(df.head(5))
+    st.write(df.head(10))
 query = st.text_area("Chat with Dataframe")
 st.write(query)
 if query:
     response = send_api_request(query)
     if isinstance(response, bytes):
         response = response.decode('utf-8') 
-    st.markdown(response)
+    st.markdown(response) 
